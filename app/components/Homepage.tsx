0ad0802382b8d7d83b6f0ac8281a2f9e804421f9
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-06 20:56:36 +0100

    wip

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { Container } from "./layout"
 
 export default function Homepage() {
--- app/components/context/WalletProvider/WalletProvider.tsx
+++ app/components/context/WalletProvider/WalletProvider.tsx
@@ -3,11 +3,11 @@
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import { alchemyProvider } from "wagmi/providers/alchemy"
 import { publicProvider } from "wagmi/providers/public"
-import { chain, createClient, configureChains, WagmiConfig, Chain } from "wagmi"
+import { chain, createClient, configureChains, WagmiConfig } from "wagmi"
 import "@rainbow-me/rainbowkit/styles.css"
 import { appName } from "app/components/layout/DefaultHead/DefaultHead"
 
-const env = String(process.env.NEXT_PUBLIC_ENV) as "mainnet" | "goerli"
+const env = String(process.env.NEXT_PUBLIC_ENV)
 const alchemyId = String(process.env.NEXT_PUBLIC_ALCHEMY_ID)
 
 const customChains = [chain[env]]
