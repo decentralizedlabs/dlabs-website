c8930c2d97d45340917bde3311490d73c84b5aa4
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-12 10:47:28 +0100

    content

--- components/ui/ConnectBlock/ConnectBlock.tsx
+++ components/ui/ConnectBlock/ConnectBlock.tsx
@@ -12,11 +12,11 @@ export default function ConnectBlock({ children, signable = false }: Props) {
     children
   ) : (
     <div className="flex flex-col items-center py-6 mx-auto max-w-screen-xs">
-      <h1>{!isConnected ? "Connect your wallet" : "Sign in"}</h1>
+      <h1>{!isConnected ? "Connect your wallet" : "Log in with wallet"}</h1>
       <p className="py-10 sm:text-lg">
         {!isConnected
           ? "Connect your wallet to view this page"
-          : "Sign in to view this page"}
+          : "Sign a message with your wallet to view this page"}
       </p>
       <CustomConnectButton signable={signable} disconnectLabel />
     </div>
