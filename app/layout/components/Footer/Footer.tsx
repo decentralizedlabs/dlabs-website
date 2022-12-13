edf9e72c0276228f79e4a04cbb3b124327eea4b0
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-13 20:52:05 +0100

    footer wip

--- app/layout/components/Footer/Footer.tsx
+++ app/layout/components/Footer/Footer.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link"
 import { Container, Social } from "../"
 
 export default function Footer() {
   return (
-    <footer className="relative z-20 py-8 text-center bg-black shadow-sm">
+    <footer className="relative z-20 pt-8 pb-4 text-center bg-black shadow-sm">
       <Container>
         <Social wrapperClassName="flex justify-center" />
+        <div className="grid items-center justify-center grid-cols-9 pt-4 mx-auto text-xs text-gray-600 w-72">
+          <Link href="/privacy" className="col-span-4 text-gray-400">
+            Privacy policy
+          </Link>
+          <p className="col-span-1">·</p>
+          <Link href="/terms" className="col-span-4 text-gray-400">
+            Terms of service
+          </Link>
+        </div>
       </Container>
     </footer>
   )
