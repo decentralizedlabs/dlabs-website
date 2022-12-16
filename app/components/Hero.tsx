4571fe27a87280e863f744b0f56e2cb09df5fc9c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-16 03:40:39 +0100

    styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@components/ui"
 import Link from "next/link"
+import Work from "./Work"
 
 export default function Hero() {
   return (
-    <>
-      <div className="pb-12">
+    <div className="pt-24 sm:pt-40">
+      <div className="pb-10 sm:pb-12">
         <h1>Decentralized labs</h1>
         <p className="text-xl font-bold text-gray-400 sm:text-2xl">
           On-demand web3 development
@@ -12,9 +13,13 @@ export default function Hero() {
       </div>
       <Button label="See plans" href="/" external={true} />
       {/* TODO: Add link */}
-      <Link href="/submit" className="inline-block mt-6 font-bold highlight">
+      <Link
+        href="/submit"
+        className="inline-block mt-4 font-bold sm:mt-6 highlight"
+      >
         Submit work
       </Link>
-    </>
+      <Work />
+    </div>
   )
 }
---
+++ app/components/Work.tsx
@@ -0,0 +1,7 @@
+export default function Work() {
+  return (
+    <div className="pt-24 opacity-60">
+      <p className="pb-12 text-lg">Our work</p>
+    </div>
+  )
+}
