6e4a2eabbc6fb9d08229e3662432aa1dc81bebec
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-19 02:04:16 +0100

    disabled discord webhook out of production

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -10,19 +10,23 @@ export default async function handler(
     if (req.method === "GET") {
       const { discord } = req.query
 
-      // Push notifiction to Discord
-      await fetch(
-        `https://discord.com/api/webhooks/${process.env.DISCORD_WEBHOOK_ID}/${process.env.DISCORD_WEBHOOK_TOKEN}`,
-        {
-          body: JSON.stringify({
-            content: `New job submitted! ${discord ? `User: @${discord}` : ""}`
-          }),
-          headers: {
-            "Content-Type": "application/json"
-          },
-          method: "POST"
-        }
-      )
+      if (process.env.NEXT_PUBLIC_ENV === "mainnet") {
+        // Push notifiction to Discord
+        await fetch(
+          `https://discord.com/api/webhooks/${process.env.DISCORD_WEBHOOK_ID}/${process.env.DISCORD_WEBHOOK_TOKEN}`,
+          {
+            body: JSON.stringify({
+              content: `New job submitted! ${
+                discord ? `User: @${discord}` : ""
+              }`
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            },
+            method: "POST"
+          }
+        )
+      }
 
       res.status(200).json({ success: true })
     }
