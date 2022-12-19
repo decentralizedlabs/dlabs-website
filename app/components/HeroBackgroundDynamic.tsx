72756a3c960140b66fea4d2f0433c99d0d0cf810
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-19 17:04:15 +0100

    removed animation from mobile

--- app/components/HeroBackgroundDynamic.tsx
+++ app/components/HeroBackgroundDynamic.tsx
@@ -29,10 +29,10 @@ export default function HeroBackgroundDynamic() {
         dynamicProps[0].fill == "transparent" ? "opacity-0" : "opacity-100"
       }`}
     >
-      {/* <path
+      <path
         d="M702 270L780.808 315.5V406.5L702 452L623.192 406.5V315.5L702 270Z"
         fill={dynamicProps[0].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[0].animationDelay
         }}
@@ -40,7 +40,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M780 675L858.808 720.5V811.5L780 857L701.192 811.5V720.5L780 675Z"
         fill={dynamicProps[1].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[1].animationDelay
         }}
@@ -48,7 +48,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M1872 135L1950.81 180.5V271.5L1872 317L1793.19 271.5V180.5L1872 135Z"
         fill={dynamicProps[2].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[2].animationDelay
         }}
@@ -56,7 +56,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M2340 675L2418.81 720.5V811.5L2340 857L2261.19 811.5V720.5L2340 675Z"
         fill={dynamicProps[3].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[3].animationDelay
         }}
@@ -64,7 +64,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M1404 945L1482.81 990.5V1081.5L1404 1127L1325.19 1081.5V990.5L1404 945Z"
         fill={dynamicProps[4].fill}
-        className="animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[4].animationDelay
         }}
@@ -72,7 +72,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M1248 405L1326.81 450.5V541.5L1248 587L1169.19 541.5V450.5L1248 405Z"
         fill={dynamicProps[5].fill}
-        className="animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[5].animationDelay
         }}
@@ -80,7 +80,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M468 945L546.808 990.5V1081.5L468 1127L389.192 1081.5V990.5L468 945Z"
         fill={dynamicProps[6].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[6].animationDelay
         }}
@@ -88,7 +88,7 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M390 540L468.808 585.5V676.5L390 722L311.192 676.5V585.5L390 540Z"
         fill={dynamicProps[7].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[7].animationDelay
         }}
@@ -96,27 +96,27 @@ export default function HeroBackgroundDynamic() {
       <path
         d="M1794 810L1872.81 855.5V946.5L1794 992L1715.19 946.5V855.5L1794 810Z"
         fill={dynamicProps[8].fill}
-        className="animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[8].animationDelay
         }}
-      /> */}
+      />
       <path
         d="M2028 405L2106.81 450.5V541.5L2028 587L1949.19 541.5V450.5L2028 405Z"
         fill={dynamicProps[9].fill}
-        className="animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[9].animationDelay
         }}
       />
-      {/* <path
+      <path
         d="M2496 135L2574.81 180.5V271.5L2496 317L2417.19 271.5V180.5L2496 135Z"
         fill={dynamicProps[0].fill}
-        className="hidden sm:block animate-pulse"
+        className="md:animate-pulse"
         style={{
           animationDelay: dynamicProps[0].animationDelay
         }}
-      /> */}
+      />
     </g>
   )
 }
