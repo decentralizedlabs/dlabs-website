318c5e34590716c6de5f24b3e8b71802b697b79f
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-19 01:27:03 +0100

    footer temp

--- app/layout/components/Footer/Footer.tsx
+++ app/layout/components/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import { Container, Social } from "../"
 
 export default function Footer() {
   return (
-    <footer className="relative z-20 pt-8 pb-4 text-center bg-black shadow-sm">
+    <footer className="relative z-20 pt-8 pb-8 text-center bg-black shadow-sm">
       <Container>
         <Social wrapperClassName="flex justify-center" />
-        <div className="grid items-center justify-center grid-cols-9 pt-4 mx-auto text-xs text-gray-600 w-72">
+        {/* <div className="grid items-center justify-center grid-cols-9 pt-4 mx-auto text-xs text-gray-600 w-72">
           <Link href="/privacy" className="col-span-4 text-right text-gray-400">
             Privacy policy
           </Link>
@@ -14,7 +14,7 @@ export default function Footer() {
           <Link href="/terms" className="col-span-4 text-left text-gray-400">
             Terms of service
           </Link>
-        </div>
+        </div> */}
       </Container>
     </footer>
   )
