c944d8cd2785591bb5963065fffba33261b180a7
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-23 02:49:08 +0100

    cleanup

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,40 +1,20 @@
 import { appUrl } from "app/layout/components/DefaultHead/DefaultHead"
-// import fetcher from "@utils/fetcher"
-// import { GetServerSideProps } from "next"
 
 function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>
+      <url>
         <loc>${appUrl}</loc>
+      </url>
+      <url>
+        <loc>${appUrl}/submit</loc>
       </url>
    </urlset>
  `
 }
 
 function SiteMap() {
-  // getServerSideProps will do the heavy lifting
   return generateSiteMap()
 }
-
-// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-//   const baseUrl = process.env.NEXT_PUBLIC_APP_URL
-//   // const { dynamicContent } = await fetcher(`${baseUrl}/`)
-
-//   // We generate the XML sitemap with the posts data
-//   // const sitemap = generateSiteMap(dynamicContent)
-
-//   // or with static data
-//   const sitemap = generateSiteMap()
-
-//   res.setHeader("Content-Type", "text/xml")
-//   // we send the XML to the browser
-//   res.write(sitemap)
-//   res.end()
-
-//   return {
-//     props: {}
-//   }
-// }
 
 export default SiteMap
